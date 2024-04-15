84a17667a3d026dea6fa99bd4d2a6d7c58170f51
mujahidina <mujahidabdi5070@gmail.com>
2024-04-15 11:03:09 +0300

    still fixing some bugs

--- server/app.py
+++ server/app.py
@@ -24,6 +24,60 @@ db.init_app(app)
 def index():
     return '<h1>Code challenge</h1>'
 
+@app.route('/restaurants')
+def get_restaurants():
+    restaurants_list = [restaurant.to_dict() for restaurant in Restaurant.query.all()]
+    return make_response(restaurants_list, 200)
 
+
+@app.route('/restaurants/<int:id>', methods = ['GET', 'DELETE'])
+def get_by_id(id):
+    if request.method == 'GET':
+        restaurant = Restaurant.query.filter(Restaurant.id == id).first()
+        if restaurant:
+            return make_response(restaurant.to_dict(), 200)
+        return make_response({
+        "error": "Restaurant not found"
+        }, 404)
+    
+
+    elif request.method == 'DELETE':
+        restaurant = Restaurant.query.filter(Restaurant.id == id).first()
+        if restaurant:
+            db.session.delete(restaurant)
+            db.session.commit()
+            return {}, 200
+        return make_response({
+        " error": "Restaurant not found"
+         }, 404)
+
+    
+
+@app.route('/pizzas')
+def get_pizzas():
+    pizza_list = [pizza.to_dict() for pizza in Pizza.query.all()]
+    return make_response(pizza_list, 200)
+
+
+
+@app.route('/restaurant_pizzas', methods=['GET', 'POST'])
+def get_rp():
+
+    if request.method == 'GET':
+        restaurant_pizza_list = [restaurant_pizza.to_dict() for restaurant_pizza in RestaurantPizza.query.all()]
+        return make_response(restaurant_pizza_list, 200)
+    
+    elif request.method == 'POST':
+        new_rp = RestaurantPizza(
+            price = request.json.get('price')
+        )
+        db.session.add(new_rp)
+        db.session.commit()
+
+        if new_rp:
+            return make_response(new_rp.to_dict(), 200)
+        return make_response({
+        "errors": ["validation errors"]
+        }, 404)
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -19,8 +19,10 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='restaurant', cascade="all,delete-orphan")
 
     # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.restaurant', '-restaurant_pizzas.pizza',)
 
     def __repr__(self):
         return f'<Restaurant {self.name}>'
@@ -34,8 +36,10 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='pizza', cascade="all,delete-orphan")
 
     # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.pizza',  '.restaurant_pizzas.restaurant',)
 
     def __repr__(self):
         return f'<Pizza {self.name}, {self.ingredients}>'
@@ -46,12 +50,22 @@ class RestaurantPizza(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
+    restaurant_id = db.Column(db.Integer(), db.ForeignKey('restaurants.id', ondelete = 'CASCADE'))
+    pizza_id = db.Column(db.Integer(), db.ForeignKey('pizzas.id', ondelete = 'CASCADE'))
 
     # add relationships
-
+    restaurant = db.relationship('Restaurant', back_populates='restaurant_pizzas')
+    pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
+     
     # add serialization rules
+    serialize_rules = ('-restaurant.restaurant_pizzas',)
 
     # add validation
+    @validates('price')
+    def validates_price(self, key, value):
+        if 1 <= value <= 30:
+            return value
+        raise ValueError('Price must be between 1 and 30')   
 
     def __repr__(self):
         return f'<RestaurantPizza ${self.price}>'
